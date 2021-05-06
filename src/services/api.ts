eec0aec32ca4f5a2e52305c16c70ba15ad91f64b
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-05-05 23:01:37 -0300

    GET API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,16 @@
 import axios from 'axios';
 
-export const api = axios.create({
-    baseURL: 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={3eb2e75941c6df4c25d84c52b8098be1}'
-})
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = '3eb2e75941c6df4c25d84c52b8098be1';
+
+export const api = async (query: string) => {
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: API_KEY,
+        }
+    });
+
+    return data;
+}
